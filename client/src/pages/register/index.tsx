11aa15ef9e78c262f50e0b1fcc3e2ef9eada7d46
tejal-supe <supetejalc@gmail.com>
2025-09-26 18:26:23 +0530

    Api integration

--- client/src/pages/register/index.tsx
+++ client/src/pages/register/index.tsx
@@ -2,41 +2,71 @@ import React, { useState } from "react";
 import { Label } from "../../components/ui/label";
 import { Input } from "../../components/ui/input";
 import { cn } from "@/lib/utils";
-import {
-  IconBrandGoogle,
-} from "@tabler/icons-react";
+import type { User } from "@/types/userType";
+import { createUser } from "@/api/userApi";
 
 const Register = () => {
-    const [state,setState] = useState({});
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log("Form submitted");
+    const [state,setState]  = useState<User>({name:"",emailId:"",password:""});
+
+    const handleChange = (e:React.ChangeEvent<HTMLInputElement>)=>{
+      const {name,value} = e.target;
+      setState({...state,[name]:value})
+    }
+
+  const handleSubmit = async(e: React.FormEvent<HTMLFormElement>) => {
+      try {
+        e.preventDefault();
+        const response = await createUser(state);
+        console.log(response,'response');
+       
+      } catch (error) {
+        console.log(error,'error in submit user');
+        
+      }
   };
   return (
     <div className="shadow-input mx-auto w-full max-w-md rounded-none bg-gray-100 p-4 md:rounded-2xl md:p-8 dark:bg-black mt-5">
       <h2 className="text-xl font-bold text-neutral-800 dark:text-neutral-200">
         Welcome to Formly
       </h2>
-      <p className="mt-2 max-w-sm text-sm text-neutral-600 dark:text-neutral-300">
-       Already have an account? <span className="text-blue-500 underline cursor-pointer">Login</span>
-      </p>
 
       <form className="my-8" onSubmit={handleSubmit}>
         <div className="mb-4 flex flex-col space-y-2 md:flex-row md:space-y-0 md:space-x-2">
           <LabelInputContainer>
             <Label htmlFor="name">Name</Label>
-            <Input id="name" placeholder="Tyler" type="text" />
+            <Input
+              id="name"
+              placeholder="Tyler"
+              type="text"
+              name="name"
+              value={state.name}
+              onChange={handleChange}
+            />
           </LabelInputContainer>
         </div>
         <LabelInputContainer className="mb-4">
           <Label htmlFor="emailId">Email Address</Label>
-          <Input id="emailId" placeholder="projectmayhem@fc.com" type="email" />
+          <Input
+            id="emailId"
+            placeholder="projectmayhem@fc.com"
+            type="email"
+            name="emailId"
+            value={state.emailId}
+            onChange={handleChange}
+          />
         </LabelInputContainer>
         <LabelInputContainer className="mb-4">
           <Label htmlFor="password">Password</Label>
-          <Input id="password" placeholder="••••••••" type="password" />
+          <Input
+            id="password"
+            placeholder="••••••••"
+            type="password"
+            name="password"
+            value={state.password}
+            onChange={handleChange}
+          />
         </LabelInputContainer>
-       
+
         <button
           className="group/btn relative block h-10 w-full rounded-md bg-gradient-to-br from-black to-neutral-600 font-medium text-white shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:bg-zinc-800 dark:from-zinc-900 dark:to-zinc-900 dark:shadow-[0px_1px_0px_0px_#27272a_inset,0px_-1px_0px_0px_#27272a_inset] cursor-pointer"
           type="submit"
@@ -46,21 +76,10 @@ const Register = () => {
         </button>
 
         <div className="my-8 h-[1px] w-full bg-gradient-to-r from-transparent via-neutral-300 to-transparent dark:via-neutral-700" />
-
-        <div className="flex flex-col space-y-4">
-   
-          <button
-            className="group/btn shadow-input relative flex h-10 w-full items-center justify-start space-x-2 rounded-md bg-gray-50 px-4 font-medium text-black dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_#262626]"
-            type="submit"
-          >
-            <IconBrandGoogle className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
-            <span className="text-sm text-neutral-700 dark:text-neutral-300">
-              Google
-            </span>
-            <BottomGradient />
-          </button>
-        
-        </div>
+        <p className="mt-2 max-w-sm text-sm text-neutral-600 dark:text-neutral-300">
+          Already have an account?{" "}
+          <span className="text-blue-500 underline cursor-pointer">Login</span>
+        </p>
       </form>
     </div>
   );
