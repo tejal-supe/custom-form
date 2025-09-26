479440352f73cc75b3e6c60dae39e66418f60cd3
tejal-supe <supetejalc@gmail.com>
2025-09-26 18:29:02 +0530

    UI bugfix

--- client/src/components/ui/resizable-navbar.tsx
+++ client/src/components/ui/resizable-navbar.tsx
@@ -164,7 +164,7 @@ export const MobileNav = ({ children, className, visible }: MobileNavProps) => {
         damping: 50,
       }}
       className={cn(
-        "relative z-50 mx-auto flex w-full max-w-[calc(100vw-2rem)] flex-col items-center justify-between bg-transparent px-0 py-2 lg:hidden",
+        "relative z-50 mx-auto flex w-full max-w-[calc(100vw-2rem)] flex-col items-center justify-between bg-transparent px-0 py-2 md:hidden",
         visible && "bg-white/80 dark:bg-neutral-950/80",
         className,
       )}
