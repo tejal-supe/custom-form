090037282afd2589981522d475c2a4c020ac0e22
tejal-supe <supetejalc@gmail.com>
2025-09-26 18:25:49 +0530

    Axios setup and api defination

---
+++ client/src/api/userApi.ts
@@ -0,0 +1,11 @@
+import { apiClient } from "@/config/client";
+import type { User } from "@/types/userType";
+
+export const createUser = async (data: User) => {
+  try {
+    const response = await apiClient.post(`/v1/user/register`, data);
+    return response.data;
+  } catch (error) {
+    console.error("Error Creating User",error);
+  }
+};
---
+++ client/src/config/client.ts
@@ -0,0 +1,17 @@
+import axios, { type AxiosInstance, type AxiosResponse } from "axios";
+
+export const apiClient: AxiosInstance = axios.create({
+  baseURL: "http://localhost:5000/api",
+  timeout: 1000,
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+apiClient.interceptors.response.use((response: AxiosResponse) => response,(error)=>{
+    if(error.response?.status === 401){
+        window.location.href = '/login'
+    }
+    return Promise.reject(error)
+});
